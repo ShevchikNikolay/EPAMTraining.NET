8f4555b57f80c0d355a19103ace9bf8dda2a042e
Николай Шевчик <vedunw@gmail.com>
2019-11-14 18:12:41 +0300

    Добавлен функционал  поиска НОД нескольких целых чисел + тесты.

--- Task1.Tests/GCDCalculatorTests.cs
+++ Task1.Tests/GCDCalculatorTests.cs
@@ -7,7 +7,7 @@ namespace Task1.Tests
     public class GCDCalculatorTests
     {
         [TestMethod]
-        public void EucldeanGCD_50and30_10returned()
+        public void EucldeanGCD_CorrectInput_10returned()
         {
             #region arrange
 
@@ -36,8 +36,7 @@ namespace Task1.Tests
         }
 
         [TestMethod]
-
-        public void EuclideanGCD_OnInput0_0returned()
+        public void EuclideanGCD_Input0_0returned()
         {
             #region arrange
 
@@ -64,5 +63,66 @@ namespace Task1.Tests
 
             #endregion
         }
+
+        [TestMethod]
+        public void EuclideanGCD_CorrectInputForMultipleNumbers_2returned()
+        {
+            #region arrange
+
+            int a = 500;
+            int b = 34;
+            int c = 156;
+            int expected = 2;
+
+            #endregion
+
+
+
+            #region act
+
+            GCDCalculator calc = new GCDCalculator();
+            int actual = calc.EuclideanGCD(a, b, c);
+
+            #endregion
+
+
+
+            #region assert
+
+            Assert.AreEqual(expected, actual);
+
+            #endregion
+        }
+
+        [TestMethod]
+        public void EuclideanGCD_Input0ForMultipleNumbers_0returned()
+        {
+            #region arrange
+
+            int a = 500;
+            int b = 34;
+            int c = 0;
+            int expected = 0;
+
+            #endregion
+
+
+
+            #region act
+
+            GCDCalculator calc = new GCDCalculator();
+            int actual = calc.EuclideanGCD(a, b, c);
+
+            #endregion
+
+
+
+            #region assert
+
+            Assert.AreEqual(expected, actual);
+
+            #endregion
+        }
+
     }
 }
--- Task1/GCDCalculator.cs
+++ Task1/GCDCalculator.cs
@@ -5,12 +5,13 @@ using System.Text;
 namespace Task1
 {
     public class GCDCalculator
-    {/// <summary>
-    /// Вычисляет НОД двух целых чисел по Алгоритму Евклида. В случае ввода 0 возвращает 0.
-    /// </summary>
-    /// <param name="a"></param>
-    /// <param name="b"></param>
-    /// <returns></returns>
+    {
+        /// <summary>
+        /// Вычисляет НОД двух целых чисел по Алгоритму Евклида. Если одно из чисел 0 - возвращает 0.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int EuclideanGCD(int a, int b)
         {
             if (a == 0 || b == 0)
@@ -30,6 +31,54 @@ namespace Task1
                 }
             }
             return (a + b);
+        }
+
+        /// <summary>
+        ///Вычисляет НОД трех целых чисел по Алгоритму Евклида. Если одно из чисел 0 - возвращает 0.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public int EuclideanGCD(int a, int b, int c)
+        {
+            int nod = EuclideanGCD(a, b);
+            nod = EuclideanGCD(c, nod);
+            return nod;
+        }
+
+        /// <summary>
+        /// Вычисляет НОД четырех целых чисел по Алгоритму Евклида. Если одно из чисел 0 - возвращает 0.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public int EuclideanGCD(int a, int b, int c, int d)
+        {
+            int nod = EuclideanGCD(a, b);
+            nod = EuclideanGCD(c, nod);
+            nod = EuclideanGCD(d, nod);
+            return nod;
+        }
+
+        /// <summary>
+        /// Вычисляет НОД пяти целых чисел по Алгоритму Евклида. Если одно из чисел 0 - возвращает 0.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        public int EuclideanGCD(int a, int b, int c, int d, int e)
+        {
+            int nod = EuclideanGCD(a, b);
+            nod = EuclideanGCD(c, nod);
+            nod = EuclideanGCD(d, nod);
+            nod = EuclideanGCD(e, nod);
+            return nod;
         }
     }
 }
