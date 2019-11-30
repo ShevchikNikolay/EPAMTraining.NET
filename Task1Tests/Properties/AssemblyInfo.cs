6da0964e78f3ea253d7cd2e5219c48df309b7b1e
Николай Шевчик <vedunw@gmail.com>
2019-11-30 06:48:08 +0300

    Fixed platform version (4.5 now)

--- Task1Tests/Properties/AssemblyInfo.cs
+++ Task1Tests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Task1.Tests")]
+[assembly: AssemblyTitle("Task1Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task1.Tests")]
+[assembly: AssemblyProduct("Task1Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("9d72ddee-0648-440d-bff3-d0e2c0e3dd42")]
+[assembly: Guid("6330ae60-8d1f-46cf-b6de-4b05b6f3e47a")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
