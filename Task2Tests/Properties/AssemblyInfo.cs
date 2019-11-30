2849be8fe8ff4f7b6ce744952172e616e0a5ad1e
Николай Шевчик <vedunw@gmail.com>
2019-11-30 06:54:28 +0300

    Fixed platform version (4.5 now)

--- Task2Tests/Properties/AssemblyInfo.cs
+++ Task2Tests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Task1.Tests")]
+[assembly: AssemblyTitle("Task2Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task1.Tests")]
+[assembly: AssemblyProduct("Task2Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("9d72ddee-0648-440d-bff3-d0e2c0e3dd42")]
+[assembly: Guid("9a4aaf31-6562-40b6-9e6f-022fdc142662")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
