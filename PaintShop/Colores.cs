78619bd345073801e748b7ae9a29eb99a00db91d
Николай Шевчик <vedunw@gmail.com>
2019-12-08 17:13:59 +0300

    Add PaintShop library project.     Add Colores enum.     Add IPaintable interface.     Add custom PaintingException.

---
+++ PaintShop/Colores.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintShop
+{
+    public enum Colores
+    {
+        Transparent,
+        Colorless,
+        Red,
+        Green,
+        Blue
+    }
+}
---
+++ PaintShop/IPaintable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintShop
+{
+    public interface IPaintable
+    {
+        void Paint(Colores color);
+    }
+}
