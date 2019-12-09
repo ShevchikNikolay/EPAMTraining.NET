7fc8efdd939524269daf6a479c9efa96f7057e68
Николай Шевчик <vedunw@gmail.com>
2019-12-09 05:34:39 +0300

    Add Geometry project.     Add abstract class Shape.     Add Rectangle class.

---
+++ Geometry/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+
+    public class Rectangle: Shape
+    {
+        public double Width { get; private set; }
+        public double Height { get; private set; }
+
+
+        public Rectangle(double width, double height, Material material): base(material)
+        {
+            Width = width;
+            Height = height;
+            Area = CalculateArea();
+            Perimeter = CalculatePerimeter();
+        }
+
+        protected override double CalculateArea()
+        {
+            return (Width * Height);
+        }
+
+        protected override double CalculatePerimeter()
+        {
+            return ((Width + Height) * 2);
+        }
+    }
+}
---
+++ Geometry/Shape.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PaintShop;
+
+namespace Geometry
+{
+    public enum Material
+    {
+        Film,
+        Paper
+    }
+    public abstract class Shape: IPaintable
+    {
+        public double Area { get; set; }
+        public double Perimeter { get; set; }
+        public Colores Color { get; private set; }
+
+        public Shape(Material material)
+        {
+            if (material == Material.Film)
+            {
+                Color = Colores.Transparent;
+            }
+            else
+            {
+                Color = Colores.Colorless;
+            }
+        }
+
+
+        
+        public void Paint(Colores color)
+        {
+            if (color == Colores.Transparent || color == Colores.Colorless)
+            {
+                throw new PaintingException("Wrong color.");
+            }
+            if (Color == Colores.Transparent)
+            {
+                throw new PaintingException("Wrong material.");
+            }
+            if (Color != Colores.Colorless)
+            {
+                throw new PaintingException("It can't be painting twice...");
+            }
+            else
+            {
+                Color = color;
+            }
+        }
+
+        protected abstract double CalculateArea();
+        protected abstract double CalculatePerimeter();
+    }
+}
