d560d6adf05b29b668253dea2e1edd7ad2b24a7c
Николай Шевчик <vedunw@gmail.com>
2019-12-13 02:44:36 +0300

    Circle and Triangle classes added. Rectangle and Shape classes edited.

---
+++ Geometry/Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; }
+        public Circle(double radius, Material material) : base(material)
+        {
+            Radius = radius;
+        }
+
+        protected override double CalculateArea()
+        {
+            return (Math.PI * Radius * Radius);
+        }
+
+        protected override double CalculatePerimeter()
+        {
+            return (Math.PI * 2 * Radius);
+        }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if(!(obj is Circle circle))
+            {
+                return false;
+            }
+            
+            return (Radius == circle.Radius);
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return (Radius.GetHashCode()*31);
+        }
+    }
+}
---
+++ Geometry/Triangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public class Triangle : Shape
+    {
+        public double FirstSide { get; }
+        public double SecondSide { get; }
+        public double ThirdSide { get; }
+
+        public Triangle(Material material, double firstSide, double secondSide, double thirdSide) : base(material)
+        {
+            FirstSide = firstSide;
+            SecondSide = secondSide;
+            ThirdSide = thirdSide;
+        }
+
+        protected override double CalculateArea()
+        {
+            var p = Perimeter / 2;
+
+            return (Math.Sqrt(p * (p - FirstSide) * (p - SecondSide) * (p - ThirdSide)));
+        }
+
+        protected override double CalculatePerimeter()
+        {
+            return (FirstSide + SecondSide + ThirdSide);
+        }
+    }
+}
