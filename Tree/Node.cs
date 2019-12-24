dcfa2671bf4c73efb22b119be7dbe584da761dfa
Николай Шевчик <vedunw@gmail.com>
2019-12-24 03:20:40 +0300

    Add project Tree.     add Node class.

---
+++ Tree/Node.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class Node<T> where T : IComparable<T>
+    {
+        T Value { get; set; }
+        Node<T> Parent { get; set; }
+        Node<T> LeftChild { get; set; }
+        Node<T> RightChild { get; set; }
+
+
+
+
+
+        public Node(T value)
+        {
+            Value = value;
+            Parent = null;
+            LeftChild = null;
+            RightChild = null;
+        }
+
+
+
+
+
+        public static bool operator >(Node<T> left, Node<T> right)
+        {
+            if (left.Value.CompareTo(right.Value) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator <(Node<T> left, Node<T> right)
+        {
+            if (left.Value.CompareTo(right.Value) > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
