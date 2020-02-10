299e763b2283faca336ad06b678792cddb87a836
Николай Шевчик <vedunw@gmail.com>
2020-02-10 06:16:28 +0300

    SessionReports methodes added

--- LinqToSQL/SessionReport.cs
+++ LinqToSQL/SessionReport.cs
@@ -6,22 +6,76 @@ using System.Threading.Tasks;
 
 namespace LinqToSQL
 {
-    class SessionReport
+    public class SessionReport
     {
-        MapingDataContext dbContext;
         
-        void getSummary(int sessionId)
+        public void GetSummaryByGrope(int sessionId)
         {
-            dbContext = new MapingDataContext();
-            var summary = from s in dbContext.Session
-                          join e in dbContext.Exam on s.id equals e.sessionId
-                          join r in dbContext.Result on e.id equals r.examId
-                          join st in dbContext.Student on r.studentId equals st.id
-                          join g in dbContext.Group on st.groupId equals g.id
-                          where s.id == sessionId select new { name = g.name};
-                          
-                          
-                       
+            using (var db = new MapingDataContext())
+            {
+                
+                var report = from Result in db.Result
+                             where
+                               Result.Exam.Session.id == sessionId
+                             group new { Result.Student.Group, Result } by new
+                             {
+                                 Result.Student.Group.name
+                             } into g
+                             select new
+                             {
+                                 AvgMark = (double?)g.Average(p => p.Result.mark),
+                                 Minimal = (int?)g.Min(p => p.Result.mark),
+                                 Maximal = (int?)g.Max(p => p.Result.mark),
+                                 GroupName = g.Key.name
+                             };
+            }
+        }
+
+        public void GetSummaryBySpecialty(int sessionId)
+        {
+            using(var db= new MapingDataContext())
+            {
+                var report = from Result in db.Result
+                             where
+                               Result.Exam.Session.id == sessionId
+                             group new { Result.Student.Group, Result } by new
+                             {
+                                 Result.Student.Group.specialty
+                             } into g
+                             select new
+                             {
+                                 AvgMark = (double?)g.Average(p => p.Result.mark),
+                                 g.Key.specialty
+                             };
+            }
+        }
+
+        public void GetSummaryByExaminer(int sessionId)
+        {
+            using (var db = new MapingDataContext())
+            {
+
+                var report = from Result in db.Result
+                             where
+                               Result.Exam.Session.id == sessionId
+                             group new { Result.Exam.Session, Result.Exam.Examner, Result } by new
+                             {
+                                 Result.Exam.Session.id,
+                                 Result.Exam.Examner.lastName,
+                                 Result.Exam.Examner.firstName,
+                                 Result.Exam.Examner.patronymic
+                             } into g
+                             orderby
+                               g.Key.id
+                             select new
+                             {
+                                 g.Key.id,
+                                 AvgMark = (double?)g.Average(p => p.Result.mark),
+                                 g.Key.lastName,
+                                 g.Key.firstName,
+                                 g.Key.patronymic
+                             };
+            }
         }
     }
 }
